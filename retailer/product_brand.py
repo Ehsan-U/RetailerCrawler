67e4e26467fb64682d66ada6f71e28096cebe74c
xgrn <xgrn119@gmail.com>
2024-02-29 22:00:02 +0300

    Brand manager implementation and testing. The class allows to save deduplicated brand names into the DB and handles existent product migrations.

---
+++ retailer/product_brand.py
@@ -0,0 +1,86 @@
+from mysql.connector.connection import MySQLConnectionAbstract
+from mysql.connector.pooling import PooledMySQLConnection
+from mysql.connector.cursor import MySQLCursorAbstract
+from unidecode import unidecode
+import re
+
+
+def normalise_name(name: str) -> str:
+    name = unidecode(name).lower().replace('&', 'and')
+    return re.sub(r'[^0-9a-z]', '', name)
+
+
+class ProductBrand:
+    db: MySQLConnectionAbstract | PooledMySQLConnection
+    cursor: MySQLCursorAbstract
+
+    def __init__(self, conn: MySQLConnectionAbstract | PooledMySQLConnection):
+        self.db = conn
+        self.cursor = conn.cursor()
+
+    def get_brand(self, name: str) -> int:
+        norm_name = normalise_name(name)
+
+        self.cursor.execute(
+            'INSERT IGNORE INTO product_brand_name(displayed_name, normalised_name) VALUES (%s, %s)',
+            (name, norm_name)
+        )
+        brand_id = self.cursor.lastrowid
+        if brand_id != 0:
+            return brand_id
+
+        self.cursor.execute('SELECT id FROM product_brand_name WHERE normalised_name = %s', (norm_name,))
+        res = self.cursor.fetchall()
+        return res[0][0]
+
+    def update_existent(self):
+        self.cursor.execute('SELECT id, brandname FROM product')
+
+        data = []
+        for row in self.cursor.fetchall():
+            data.append(row)
+
+        self.__make_tmp_table()
+
+        batch = 0
+        brands = []
+        products = []
+        for (product_id, brand_name) in data:
+            norm_name = normalise_name(brand_name)
+            brands.extend([brand_name, norm_name])
+            products.extend([product_id, norm_name])
+            batch += 1
+            if batch > 50:
+                self.__flush_batch(brands, products)
+                batch = 0
+                brands = []
+                products = []
+
+        self.__flush_batch(brands, products)
+        self.__finalise_update()
+
+    def __make_tmp_table(self):
+        tmp_cursor = self.db.cursor()
+        tmp_cursor.execute('CREATE TEMPORARY TABLE tmp_product_brand (product_id INT, brand_name VARCHAR(64))')
+
+    def __finalise_update(self):
+        insert_cursor = self.db.cursor()
+        insert_cursor.execute('''
+            UPDATE product AS p
+                   JOIN tmp_product_brand AS tpb ON tpb.product_id = p.id
+                   JOIN product_brand_name AS pbn ON pbn.normalised_name = tpb.brand_name
+            SET p.related_brand_name_id = pbn.id
+        ''')
+        insert_cursor.execute('DROP TEMPORARY TABLE tmp_product_brand')
+
+    def __flush_batch(self, brands: list, products: list):
+        if len(brands) == 0 or len(products) == 0:
+            return
+        insert_cursor = self.db.cursor()
+        query = 'INSERT IGNORE INTO product_brand_name(displayed_name, normalised_name) VALUES '
+        query += '(%s, %s),' * int(len(brands) / 2)
+        insert_cursor.execute(query.rstrip(','), brands)
+
+        query = 'INSERT INTO tmp_product_brand(product_id, brand_name) VALUES '
+        query += '(%s, %s),' * int(len(products) / 2)
+        insert_cursor.execute(query.rstrip(','), products)
---
+++ tests/test_product_brand.py
@@ -0,0 +1,60 @@
+from tests.abstract_test_case import AbstractTestCase
+from retailer.product_brand import ProductBrand
+from retailer.product_brand import normalise_name
+
+
+class TestProductBrand(AbstractTestCase):
+    def test_get_brand(self):
+        manager = ProductBrand(self.db)
+
+        brand_id = manager.get_brand('My Brand')
+        self.assertIsInstance(brand_id, int)
+
+        another_brand_id = manager.get_brand('Another Brand')
+        self.assertIsInstance(another_brand_id, int)
+        self.assertNotEqual(brand_id, another_brand_id)
+
+        dup_brand = manager.get_brand('my_brand')
+        self.assertEqual(brand_id, dup_brand)
+
+    def test_normalisation(self):
+        self.assertEqual('mybrand', normalise_name('My Brand'))
+        self.assertEqual('myandbrand', normalise_name('My & Brand'))
+        self.assertEqual('myssraeiu', normalise_name('MÝ ßRæïü'))
+
+    def test_product_update(self):
+        fixtures = self.db.cursor()
+
+        fixtures.execute('''
+                INSERT INTO user(
+                    firstname, lastname, email, password, roles, city,
+                    created_at, status, email_validation_token, website,
+                    logo
+                )
+                VALUES ('', '', 'user@test.te', '', '[]', '', CURRENT_TIMESTAMP, '', '', '', '')
+        ''')
+
+        fixtures.execute('''
+                INSERT INTO product(
+                    title, user_id, brandname, url, description, price,
+                    discount, status, created_at, updated_at, discounted_price
+                )
+                VALUES ('Prod1', %s, 'My Brand', '', '', 0, 0, '', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, 0),
+                       ('Prod2', %s, 'my_brand', '', '', 0, 0, '', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, 0),
+                       ('Prod3', %s, 'Another Brand', '', '', 0, 0, '', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, 0)
+            ''',
+            (fixtures.lastrowid, fixtures.lastrowid, fixtures.lastrowid)
+        )
+        manager = ProductBrand(self.db)
+        manager.update_existent()
+
+        fixtures.execute('''
+            SELECT p.title, b.displayed_name
+            FROM product AS p
+                 JOIN product_brand_name AS b ON b.id = p.related_brand_name_id
+        ''')
+        for (title, brand_name) in fixtures.fetchall():
+            if title == 'Prod1' or title == 'Prod2':
+                self.assertEqual('My Brand', brand_name)
+            else:
+                self.assertEqual('Another Brand', brand_name)
