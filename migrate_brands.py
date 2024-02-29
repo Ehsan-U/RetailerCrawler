f2ec54df4f01c3a175fbf58ca064c79105be96fc
xgrn <xgrn119@gmail.com>
2024-02-29 22:01:16 +0300

    Brand name migration script added.

---
+++ migrate_brands.py
@@ -0,0 +1,18 @@
+from mysql.connector import connect
+from dotenv import load_dotenv
+import os
+from retailer.product_brand import ProductBrand
+
+if __name__ == '__main__':
+    load_dotenv()
+    db = connect(
+        host=os.getenv('DB_HOST'),
+        user=os.getenv('DB_USER'),
+        password=os.getenv('DB_PASSWORD'),
+        database=os.getenv('DB_NAME'),
+        port=os.getenv('DB_PORT'),
+        use_pure=False
+    )
+    manager = ProductBrand(db)
+    manager.update_existent()
+    db.close()
